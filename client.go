d266acd86f40872a9b4219a23b9fb34d8ac7fa3f
unknown <kang.fu@epfl.ch>
2022-11-29 08:15:04 +0100

    updated

--- client.go
+++ client.go
@@ -8,83 +8,107 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("./client port name")
+	if len(os.Args) < 4 {
+		fmt.Println("./client port name target")
 	}
 	name := os.Args[2]
+	target := os.Args[3]
 	port, _ := strconv.Atoi(os.Args[1])
-	localAddr := net.UDPAddr{Port: port,}
-	remoteAddr := net.UDPAddr{
-		IP: net.ParseIP("20.203.137.35"),
+	localAddr := net.UDPAddr{Port: port}
+	registerRemoteAddr := net.UDPAddr{
+		IP:   net.ParseIP("10.20.3.135"),
+		Port: 8081,
+	}
+	targetRemoteAddr := net.UDPAddr{
+		IP:   net.ParseIP("10.20.3.135"),
 		Port: 8080,
 	}
-
-	conn, err := net.DialUDP("udp",&localAddr, &remoteAddr)
-	if err != nil{
-		log.Panic("failed to DialUDP",err)
-	}
-	conn.Write([]byte("I am a peer: " + name))
-
-	buf := make([]byte,256)
-	n, _ , err := conn.ReadFromUDP(buf)
-	if err != nil{
-		log.Panic("failed to ReadFromUDP",err)
-	}
-	toAddr:= parseIP(string(buf[:n]))
-
+	register(localAddr, registerRemoteAddr, name)
+	time.Sleep(10 * time.Second)
+	toAddr := getDestAddr(localAddr, targetRemoteAddr, name, target)
 	fmt.Println("target addr", toAddr)
-	conn.Close()
-
 	p2pchat(&localAddr, &toAddr)
 }
 
-func parseIP(addr string) (net.UDPAddr){
+func parseIP(addr string) net.UDPAddr {
 	strs := strings.Split(addr, ":")
 	// if len(strs) != 2 {
 	// 	fmt.Println("ip addr is not valid")
 	// 	return nil , errors.New("ipaddr err")
 	// }
-
 	ip := strs[0]
 	port, _ := strconv.Atoi(strs[1])
 	return net.UDPAddr{
-		IP: net.ParseIP(ip),
+		IP:   net.ParseIP(ip),
 		Port: port,
 	}
+}
 
+func register(localAddr, remoteAddr net.UDPAddr, source string) {
+	conn, err := net.DialUDP("udp", &localAddr, &remoteAddr)
+	if err != nil {
+		log.Panic("failed to DialUDP", err)
+	}
+	conn.Write([]byte("This is peer:" + source))
+	buf := make([]byte, 256)
+	n, _, err := conn.ReadFromUDP(buf)
+	if err != nil {
+		log.Panic("failed to ReadFromUDP", err)
+	}
+	fmt.Printf("%s", buf[:n])
+	conn.Close()
+}
+
+func getDestAddr(localAddr, remoteAddr net.UDPAddr, source, dest string) net.UDPAddr {
+	conn, err := net.DialUDP("udp", &localAddr, &remoteAddr)
+	if err != nil {
+		log.Panic("failed to DialUDP", err)
+	}
+	conn.Write([]byte(source + ":" + dest))
+	fmt.Println("sending target")
+
+	buf := make([]byte, 256)
+	n, _, err := conn.ReadFromUDP(buf)
+	if err != nil {
+		log.Panic("failed to ReadFromUDP", err)
+	}
+	toAddr := parseIP(string(buf[:n]))
+	conn.Close()
+	return toAddr
 }
 
 func p2pchat(fromAddr, toAddr *net.UDPAddr) {
 	conn, err := net.DialUDP("udp", fromAddr, toAddr)
-	if err != nil{
-		log.Panic("failed to DialUDP",err)
+	if err != nil {
+		log.Panic("failed to DialUDP", err)
 	}
 	n, err := conn.Write([]byte("Hole punching \n"))
-	fmt.Println(n,err)
+	fmt.Println(n, err)
 
-	// goroutine handle 
-	go func()  {
+	// goroutine handle
+	go func() {
 		buf := make([]byte, 256)
-		for{
-			n , _ , err := conn.ReadFromUDP(buf)
-			if err != nil{
-				fmt.Println("readFromUDP err",err)
+		for {
+			n, _, err := conn.ReadFromUDP(buf)
+			fmt.Println("received")
+			if err != nil {
+				fmt.Println("readFromUDP err", err)
 				continue
 			}
 			fmt.Printf("receive: %sp2p> \n", buf[:n])
 		}
-		
 	}()
 
 	reader := bufio.NewReader(os.Stdin)
-	for{
+	for {
 		fmt.Println("p2p>")
 		data, err := reader.ReadString('\n')
-		if err != nil{
-			log.Panic("failed to read string",err)
+		if err != nil {
+			log.Panic("failed to read string", err)
 		}
 		conn.Write([]byte(data))
 	}
--- server.go
+++ server.go
@@ -4,40 +4,101 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"
+
+	"strings"
+	"sync"
 )
+
 // establish UDP server
 // receive 2 requests
 // exchange messages
 // quit not affect A and B commuication
+type peers struct {
+	mu   sync.RWMutex
+	addr map[string]*net.UDPAddr
+	ch   chan struct {
+		string
+		*net.UDPAddr
+	}
+}
+
 func main() {
 	fmt.Println("begin server")
-	//服务器启动侦听
-	listener, err := net.ListenUDP("udp", &net.UDPAddr{Port: 8888})
-	if err != nil{
+
+	//peers := make([]*net.UDPAddr,2)
+	p := peers{
+		addr: make(map[string]*net.UDPAddr),
+	}
+	listener, err := net.ListenUDP("udp", &net.UDPAddr{Port: 8081})
+	if err != nil {
 		log.Panic("Failed to ListenUDP", err)
 	}
-	defer listener.Close()
-	peers := make([]*net.UDPAddr,2)
-	buf := make([]byte ,256)
-	n, addr, err := listener.ReadFromUDP(buf)
+	listener1, err := net.ListenUDP("udp", &net.UDPAddr{Port: 8080})
 	if err != nil {
-		log.Panic("1 Fail to readFromUDP")
+		log.Panic("Failed to ListenUDP", err)
 	}
-	peers[0] = addr
-	fmt.Printf("read %d size, from %s, msg: %s \n",n,addr.String(),buf[:n])
-
-	n, addr, err = listener.ReadFromUDP(buf)
-	if err != nil {
-		log.Panic("2 Fail to readFromUDP")
+	go p.receiveRegistration(listener)
+	go p.findTargetAddr(listener1)
+	for {
 	}
-	peers[1] = addr
-	fmt.Printf("read %d size, from %s, msg: %s",n,addr.String(),buf[:n])
-	// Exchange messages
-	listener.WriteToUDP([]byte(peers[0].String()), peers[1])
-	listener.WriteToUDP([]byte(peers[1].String()), peers[0])
+	// n, addr, err = listener.ReadFromUDP(buf)
+	// if err != nil {
+	// 	log.Panic("2 Fail to readFromUDP")
+	// }
+	// msg = strings.Split(string(buf[:n]), ":")
+	// peers[msg[0]] = addr.String()
+	// fmt.Printf("read %d size, from %s, msg: %s",n,addr.String(),buf[:n])
+	// // Exchange messages
+	// listener.WriteToUDP([]byte(peers[0].String()), peers[1])
+	// listener.WriteToUDP([]byte(peers[1].String()), peers[0])
 	// Exit()
-	fmt.Println("the server will disconnect after 10 seconds")
-	time.Sleep(time.Second*10)
+	// fmt.Println("the server will disconnect after 10 seconds")
+	// time.Sleep(time.Second*10)
 
 }
+
+func (p *peers) receiveRegistration(listener *net.UDPConn) {
+	for {
+		buf := make([]byte, 256)
+		n, addr, err := listener.ReadFromUDP(buf)
+		if err != nil {
+			log.Panic("Fail to readFromUDP")
+		}
+		msg := strings.Split(string(buf[:n]), ":")
+		p.mu.Lock()
+		p.addr[msg[1]] = addr
+		p.mu.Unlock()
+		fmt.Printf("read %d size, from %s, msg: %s \n", n, addr.String(), buf[:n])
+
+		listener.WriteToUDP([]byte("register successful"), addr)
+	}
+
+}
+
+func (p *peers) findTargetAddr(listener *net.UDPConn) {
+	for {
+		buf := make([]byte, 256)
+		n, _, err := listener.ReadFromUDP(buf)
+		if err != nil {
+			log.Panic("Fail to readFromUDP")
+		}
+
+		msg := strings.Split(string(buf[:n]), ":")
+		for {
+			p.mu.Lock()
+			t, ok := p.addr[msg[1]]
+
+			if ok {
+				listener.WriteToUDP([]byte(t.String()), p.addr[msg[0]])
+				p.mu.Unlock()
+				return
+			}
+			p.mu.Unlock()
+		}
+	}
+}
+
+// // func (p *peers) findTargetAddr(listener *net.UDPConn){
+// // 	source, dest := p.receiveTargetAddr(listener)
+
+// // }
